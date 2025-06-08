920b151d988dcc05290e5e8f84c95de53d2e6a86
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-06-08 19:02:09 +0900

    fix: 배포시 미사용 import 문 noUnusedParameters 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
