f2c7bddcecec57bcd98a631459a5bd30a834121e
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-06-22 16:23:33 +0900

    fix: 최신글 인기글 탭 복구..

--- src/pages/MainPage/components/MainPosts.tsx
+++ src/pages/MainPage/components/MainPosts.tsx
@@ -1,6 +1,8 @@
 // import { useState } from "react";
 import MainPagePostPreview from "./MainPagePostPreview";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 
 interface MainPostsProps {
@@ -18,25 +20,58 @@ interface MainPostsProps {
 function MainPosts ({ posts }: MainPostsProps) {
 
     const navigate = useNavigate();
+    const [activeTab, setActiveTab] = useState<"latest" | "popular">("latest");
+
 
     const handleViewAllClick = () => {
-        navigate(`/view-all`);
-    };
+        navigate(`/view-all/${activeTab}`);
+      };
+      
 
     return (
         <div>
-            <div className="w-full border-t-[1.5px] border-gray-300" />
-            <div className="flex justify-center">
-                <div className="flex w-[1200px] justify-end">
-                    <button className="flex text-[16px] mt-[23px] text-gray-500 items-center "
-                        onClick = {handleViewAllClick}
+            <div className="flex justify-center mt-[-45px]">
+                <div className="flex w-[1200px] justify-start">
+                    <div className="flex gap-[8px]">
+                        <button
+                            onClick={() => setActiveTab("latest")}
+                            className={`pb-2 w-[85px] h-[45px] font-pretendard text-[24px] ${
+                            activeTab === "latest"
+                                ? "text-gray-750 font-semibold border-b-2 border-black"
+                                : "text-gray-400"
+                            }`}
                         >
-                            전체보기 ›
-                    </button>
+                            최신
+                        </button>
+                        <button
+                            onClick={() => setActiveTab("popular")}
+                            className={`pb-2 w-[85px] h-[45px] font-pretendard text-[24px] ${
+                            activeTab === "popular"
+                                ? "text-gray-750 font-semibold border-b-2 border-black"
+                                : "text-gray-400"
+                            }`}
+                        >
+                            인기
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center mt-[44px]">
+                <div className="flex w-[1200px] justify-between">
+                <h2 className="text-[38px] font-semibold text-gray-800">
+                    {activeTab === "latest" ? "최신 글" : "인기 글"}
+                </h2>
+                <button
+                    className="flex text-[16px] text-gray-500 items-center"
+                    onClick={handleViewAllClick}
+                >
+                    전체보기 ›
+                </button>
                 </div>
             </div>
             <div className="flex justify-center">
-                <div className="grid grid-cols-4 gap-x-[24px] gap-y-[54px] w-[1200px] mt-[40px] mb-[69px] items-start">
+                <div className="grid grid-cols-4 gap-x-[24px] gap-y-[54px] w-[1200px] mt-[48px] mb-[69px] items-start">
                     {posts.map((post) => (
                         <MainPagePostPreview
                             key={post.post_id}
