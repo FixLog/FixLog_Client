e80eae669832c39afc82efaecdc8e77b77060b53
rngrhn4114 <is9206855@gmail.com>
2025-06-24 20:26:02 +0900

    chore: 기타 UI 요소 조정

--- src/pages/MyPage/components/MyPageNavTabs.tsx
+++ src/pages/MyPage/components/MyPageNavTabs.tsx
@@ -27,7 +27,7 @@ function MyPageNavTabs({ onTabChange, isOwner, activeTab }: MyPageNavTabsProps) 
     <div className="flex border-b border-gray-200 mb-4 py-">
       {tabs.map((tab) => {
         const isActive = activeTab === tab.value;
-        const baseClass = "py-4 px-[15px] mr-[32.5px] text-[19px] font-medium transition-colors duration-150";
+        const baseClass = "py-4 px-[15px] mr-[32.5px] text-[19px] font-bold transition-colors duration-150";
         const activeClass = "border-b-2 border-black font-bold text-black";
         const inactiveClass = "text-gray-500 hover:text-black";
 
